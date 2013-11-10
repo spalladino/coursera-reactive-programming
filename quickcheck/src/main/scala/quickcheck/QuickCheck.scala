eed49347fd4ec98907efc359070d60cfcb0dd9bc
Santiago Palladino <spalladino@gmail.com>
2013-11-10 14:47:05 -0300

    Completed first assignment

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -1,11 +1,11 @@
 package quickcheck
 
 import common._
-
 import org.scalacheck._
 import Arbitrary._
 import Gen._
 import Prop._
+import scala.collection.immutable.SortedSet
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
@@ -38,6 +38,19 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
   	val x2 = findMin(h2)
   	val xm = findMin(meld(h1, h2))
   	if (x1 <= x2) xm == x1 else xm == x2
+  }
+  
+  property("sortedMelded") = forAll { (h1 : H, h2 : H) =>
+  	valuesSorted(Int.MinValue, meld(h1, h2))
+  }
+  
+  property("valuesMelded") = forAll { (h1 : H, h2 : H) =>
+  	(values(h1) ++ values(h2)) == values(meld(h1, h2))
+  }
+  
+  def values(h : H) : Set[Int] = isEmpty(h) match {
+    case true => Set()
+    case false => values(deleteMin(h)) + findMin(h)
   }
   
   def valuesSorted(x: Int, h: H): Boolean = isEmpty(h) match {
