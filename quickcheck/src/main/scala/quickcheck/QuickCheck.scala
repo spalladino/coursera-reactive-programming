7e5f5cfc28aedb0e5d7b5b7730730a4033ca0766
Santiago Palladino <spalladino@gmail.com>
2013-11-09 22:51:53 -0300

    First properties

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -14,7 +14,29 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
-  lazy val genHeap: Gen[H] = ???
+  property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h)) == m
+  }
+  
+  property("min2") = forAll { (a: Int, b: Int) =>
+    val h = insert(a, insert(b, empty))
+    if (a < b) findMin(h) == a else findMin(h) == b
+  }
+  
+  property("emptied") = forAll { (a: Int) =>
+    val h = deleteMin(insert(a, empty))
+    isEmpty(h)
+  }
+  
+  /*property("sorted") = forAll { (h : H) =>
+    
+  }*/
+
+  lazy val genHeap: Gen[H] = for {
+    v <- arbitrary[Int]
+    h <- oneOf(empty, genHeap)
+  } yield insert(v, h)
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
