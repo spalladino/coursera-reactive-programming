d8df9082f0db6d7f33366288375d43b37bcfe15e
Santiago Palladino <spalladino@gmail.com>
2013-11-25 15:46:26 -0300

    Listener implemented

--- nodescala/src/main/scala/nodescala/nodescala.scala
+++ nodescala/src/main/scala/nodescala/nodescala.scala
@@ -29,7 +29,13 @@ trait NodeScala {
    *  @param token        the cancellation token for
    *  @param body         the response to write back
    */
-  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = ???
+  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = {
+    for (part <- response
+        if !token.isCancelled) {
+      exchange.write(part)
+    }
+    exchange.close
+  }
 
   /** A server:
    *  1) creates and starts an http listener
@@ -111,7 +117,11 @@ object NodeScala {
      *  @param relativePath    the relative path on which we want to listen to requests
      *  @return                the promise holding the pair of a request and an exchange object
      */
-    def nextRequest(): Future[(Request, Exchange)] = ???
+    def nextRequest(): Future[(Request, Exchange)] = {
+      val p = promise[(Request, Exchange)]
+      createContext { ex => p.success(( ex.request, ex )); removeContext() }
+      p.future
+    }
   }
 
   object Listener {
